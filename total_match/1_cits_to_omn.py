ab129f5c7403fc9b7c3bc0a39aed36288744438b
romanoffnv <romanoffnv@gmail.com>
2022-10-31 19:46:51 +0300

    finalizing cits match/dismatch algo

--- total_match/1_cits_to_omn.py
+++ total_match/1_cits_to_omn.py
@@ -36,29 +36,32 @@ def main():
 
     # Destructuring cits df
     def cits_destructurer():
-        L1 = df_cits['Crews'].tolist()
-        L2 = df_cits['Units'].tolist()
-        L3 = df_cits['Plates'].tolist()
-        L4 = df_cits['Plate_index'].tolist()
-        L5 = df_cits['Locations'].tolist()
-        return L1, L2, L3, L4, L5
+        L0 = df_cits['Crews'].tolist()
+        L1 = df_cits['Units'].tolist()
+        L2 = df_cits['Plates'].tolist()
+        L3 = df_cits['Plate_index'].tolist()
+        L4 = df_cits['Locations'].tolist()
+        return L0, L1, L2, L3, L4
     
     L_all_cits = cits_destructurer()
     
     # Destructuring omnicomm df 
-    L_Plate_dept_om = df_om['Department'].tolist()
-    L_units_om = df_om['Vehicle'].tolist()
-    L_Plates = df_om['Plate'].tolist()
-    L_Plate_index_om = df_om['Plate_index'].tolist()
-    L_Locs_om = df_om['Location_Omnicomm'].tolist()
-    L_nodata_om = df_om['No_data'].tolist()
+    def om_destructurer():
+        L0 = df_om['Department'].tolist()
+        L1 = df_om['Vehicle'].tolist()
+        L2 = df_om['Plate'].tolist()
+        L3 = df_om['Plate_index'].tolist()
+        L4 = df_om['Location_Omnicomm'].tolist()
+        L5 = df_om['No_data'].tolist()
+        return L0, L1, L2, L3, L4, L5
     
+    L_all_om = om_destructurer()
     
     # Get matched plates
     def matcher(L_values):
         D = dict(zip(L_all_cits[3], L_values))
         L = []
-        for i in L_Plate_index_om:
+        for i in L_all_om[3]:
             if i in D.keys():
                 L.append(D.get(i))
             else:
@@ -72,18 +75,20 @@ def main():
     
     df_matched = pd.DataFrame(zip(
                         # Omnicomm
-                        L_Plate_dept_om,
-                        L_units_om,
-                        L_Plates,
-                        L_Plate_index_om,
-                        L_Locs_om,
-                        L_nodata_om,
+                        L_all_om[0],
+                        L_all_om[1],
+                        L_all_om[2],
+                        L_all_om[3],
+                        L_all_om[4],
+                        L_all_om[5],
+                        
                         # Cits
                         L_crews_cits,
                         L_units_cits,
                         L_Plates_cits,
                         L_Plate_index_cits,
-                        L_Locations_cits), columns= [
+                        L_Locations_cits), 
+                        columns= [
                         # Omnicomm
                         'Group', 
                         'Units_om',
@@ -96,17 +101,16 @@ def main():
                         'Units_ct',
                         'Plates_ct',
                         'PI_ct',
-                        'Locs_ct',
-                        
-                        ])
+                        'Locs_ct',]
+    )
     
-    
+    # pprint(df_matched)
     # Get unmatched plates
     def dismatcher(L_values):
         D = dict(zip(L_all_cits[3], L_values))
         L = []
         for k, v in D.items():
-            if k not in L_Plate_index_om:
+            if k not in L_all_cits[3]:
                 L.append(v)
                 
         return L
@@ -119,64 +123,57 @@ def main():
     L_Locations_cits = dismatcher(L_all_cits[4])
     
     # Blanking out omnicomm cols for unmatched items by the length of Crew col
+    
+    for i in L_all_om:
+        i.clear() 
+    pprint(L_all_om)
+    L_all_om[0].append('-')
+    pprint(L_all_om[0])
     for i in L_crews_cits:
-        L_Plate_dept_om.append('-')
-    pprint(len(L_Plate_dept_om))
-    
-    # Omnicomm
-                        # L_Plate_dept_om,
-                        # L_units_om,
-                        # L_Plates,
-                        # L_Plate_index_om,
-                        # L_Locs_om,
-                        # L_nodata_om,
-    
-    # df_unmatched = pd.DataFrame(zip(L_crews_cits, L_units_cits, L_Plates_cits, L_Plate_index_cits, L_Locations_cits))
-    
-    # L_unmatched_PInd, L_unmatched_Crews = [], []
-    # # Get umatched plates
-    # for k, v in D.items():
-    #     if k in L_Plate_index_om:
-    #         pass
-    #     else:
-    #         L_unmatched_PInd.append(k)
-    #         L_unmatched_Crews.append(v)
-    
-    
-    # pprint(len(L_unmatched_PInd))
-    # pprint(len(L_unmatched_Crews))
-    # pprint(L_matched)
-    # pprint(L_unmatched)
-    # pprint(len(L_matched))
-    # pprint(len(L_unmatched))
-    # for k, v in D.items():
-    #     if k in L_Plate_index_om:
-    #         L_matched.append(v)
-    #     else:
-    #         L_matched.append('-')
-    #         L_unmatched.append(k)
-    
-    # pprint(L_matched)
-    # pprint(L_unmatched)
-    # pprint(len(L_matched))
-    # pprint(len(L_unmatched))
-    # pprint(L_crews)
-    # Matching multiple columns via dict
-    # def dict_matcher(x):
-    #         L = []
-    #         D = dict(zip(L_total_frac_ind, x))
-    #         for i in L_total_plates_ind:
-    #             L.append(D.get(i))
-    #         return L
+        L_all_om[0].append(i)
+        L_all_om[1].append('-')
+        L_all_om[2].append('-')
+        L_all_om[3].append('-')
+        L_all_om[4].append('-')
+        L_all_om[5].append('-')
         
-    # L_group_matched = dict_matcher(L_frac_group)
-    # L_unit_matched = dict_matcher(L_frac_unit)    
-    # L_plates_matched = dict_matcher(L_frac_plates)    
-    # L_mols_matched = dict_matcher(L_frac_mols)    
-    # L_drivers_matched = dict_matcher(L_frac_drivers)    
-    # L_discrepancies_matched = dict_matcher(L_frac_discrepancies)    
-    # L_notes_matched = dict_matcher(L_frac_notes) 
-
+    # Google append to the list number of times
+    
+    pprint(L_all_om[0])
+    df_unmatched = pd.DataFrame(zip(
+                        # Omnicomm
+                        L_all_om[0],
+                        L_all_om[1],
+                        L_all_om[2],
+                        L_all_om[3],
+                        L_all_om[4],
+                        L_all_om[5],
+                        
+                        # Cits
+                        L_crews_cits,
+                        L_units_cits,
+                        L_Plates_cits,
+                        L_Plate_index_cits,
+                        L_Locations_cits), 
+                        columns= [
+                        # Omnicomm
+                        'Group', 
+                        'Units_om',
+                        'Plates_om',
+                        'PI_om',
+                        'Locs_om',
+                        'No_data',
+                        # Cits
+                        'Crews_ct',
+                        'Units_ct',
+                        'Plates_ct',
+                        'PI_ct',
+                        'Locs_ct',]
+    )
+    pprint(df_unmatched)
+    
+    
+    
 
 
 if __name__== '__main__':
