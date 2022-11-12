42c417cdf5fbb70c32e7706be83785264f672e15
romanoffnv <romanoffnv@gmail.com>
2022-11-12 21:38:35 +0300

    arby is left to get matched

--- arby/1_get_src.py
+++ arby/1_get_src.py
@@ -14,14 +14,10 @@ from collections import Counter
 import win32com
 print(win32com.__gen_path__)
 
-# # Get the Excel Application COM object
-# xl = EnsureDispatch('Excel.Application')
-# wb = xl.Workbooks.Open(f"{os.getcwd()}\\arby_ct.xlsx")
-# Sheets = wb.Sheets('Общая')
-# ws = wb.Worksheets(Sheets)
+
 
 # Making connections to DBs
-db = sqlite3.connect('brm.db')
+db = sqlite3.connect('arby.db')
 db.row_factory = lambda cursor, row: row[0]
 cursor = db.cursor()
 
@@ -51,11 +47,11 @@ def main():
         L_plates, L_drivers  = zip(*dc.items())
         L_drivers = [', '.join(x) for x in L_drivers]
         
-        df = pd.DataFrame(zip(L_drivers, L_plates), columns =['Driver', 'Plate'])
+        df = pd.DataFrame(zip(L_drivers, L_plates), columns =['Drivers', 'Plates'])
         return df
     # Turn plates into 123abc type
     def transform_plates(plates):
-        L_regions_long = [126, 156, 158, 174, 186, 188, 196, 797]
+        L_regions_long = [126, 156, 158, 174, 186, 188, 196, 197, 797]
         L_regions_short = ['01', '02', '03', '04', '05', '06', '07', '09']
         for i in L_regions_long:
             plates = [x.removesuffix(str(i)).strip() if x != None and len(x) == 9 else x for x in plates]
@@ -69,45 +65,60 @@ def main():
         plates = [str(x) + str(y) for x, y in zip(plates_numeric, plates_literal)]
         plates = [''.join(re.sub(r'\s+', '', x)).lower() for x in plates if x != None]
         return plates
-
+    def list_getter(df_type, arg):
+        L = df_type[arg].tolist()
+        return L
+    def df_maker(*args):
+        if len(args) == 2:
+            df = pd.DataFrame(zip(*args), columns = ['Plates', 'Drivers'])
+        elif len(args) == 3:
+            df = pd.DataFrame(zip(*args), columns = ['Plates', 'PI', 'Drivers'])
+        return df
     # ===================================================================================
     
     # Reading xls
-    df = xlsreader("arby_ct.xlsx")
+    df_ct = xlsreader("arby_ct.xlsx")
+    df_trans = xlsreader("arby_trans.xlsx")
     
-    # Listing columns
-    L_plates = df['Гос. №'].tolist()
-    L_drivers = df['Ответственный'].tolist()
-
+    # Listing columns, making df for ct
+    L_plates = list_getter(df_ct, 'Гос. №')
+    L_drivers = list_getter(df_ct, 'Ответственный')
+    df1 = df_maker(L_plates, L_drivers)
+    
+    # Listing columns, making df for trans
+    L_plates = list_getter(df_trans, 'Гос №')
+    L_drivers = list_getter(df_trans, 'Ответственный')
+    df2 = df_maker(L_plates, L_drivers)
+    
+    # Merging dfs
+    df = pd.merge(df1, df2, how="outer")
+    
+    # Listing plates and drivers from total df 
+    L_plates = list_getter(df, 'Plates')
+    L_drivers = list_getter(df, 'Drivers')
+    
     # Making replacements
     L_plates = replacer(L_plates)
     
-    # Preping drivers
+    # Prepping drivers
     L_drivers = driversprepper(L_drivers)
     
     # Packing dubs
-    df_ct = dubspacker(L_plates, L_drivers)
+    df = dubspacker(L_plates, L_drivers)
     
-    # Reading xls
-    df = xlsreader("arby_trans.xlsx")
+    # Listing plates and drivers after dubs were packed
+    L_plates = list_getter(df, 'Plates')
+    L_drivers = list_getter(df, 'Drivers')
     
-    # Listing columns
-    L_plates = df['Гос №'].tolist()
-    L_drivers = df['Ответственный'].tolist()
-
-     # Making replacements
-    L_plates = replacer(L_plates)
-    
-    # Preping drivers
-    L_drivers = driversprepper(L_drivers)
-    
-    # Packing dubs
-    df_trans = dubspacker(L_plates, L_drivers)
-    df = pd.merge(df_ct, df_trans, how="outer")
-
-    L_plates = df['Plate'].tolist()
+    # Transforming plates into 123abc format 
     L_PI = transform_plates(L_plates)
-    pprint(L_PI)
+    df = df_maker(L_plates, L_PI, L_drivers)
+    
+    # Post df to DB
+    cursor.execute("DROP TABLE IF EXISTS ct_drivers")
+    df.to_sql(name='ct_drivers', con=db, if_exists='replace', index=False)
+    db.commit()
+    db.close()
     
 if __name__ == '__main__':
     start_time = time.time()
