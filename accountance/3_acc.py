2d64ea62d08b8c033103ec9defb710fb4e056ab4
romanoffnv <romanoffnv@gmail.com>
2022-11-15 08:23:27 +0300

    accountance comment

--- accountance/3_acc.py
+++ accountance/3_acc.py
@@ -28,15 +28,19 @@ def main():
     xl = pd.ExcelFile('gen_report.xls')
     
     pprint(xl.sheet_names)  # see all sheet names
-
-    df1 = xl.parse('ГНКТ')
-    df1 = df1.drop(range(0, 11))
-    L_units = df1['Unnamed: 3']
-    L_comments = df1['Unnamed: 15']
-    df1 = pd.DataFrame(zip(L_units, L_comments))
+    def sheets_parser(service):
+        df = xl.parse(service)
+        df = df.drop(range(0, 11))
+        L_units = df['Unnamed: 3']
+        L_plates = df['Unnamed: 9']
+        L_comments = df['Unnamed: 15']
+        df = pd.DataFrame(zip(L_units, L_plates, L_comments))
+        return df
+    
+    df1 = sheets_parser('ГНКТ')
+    # df2 = xl.parse('ГРП')
     pprint(df1)
-    df2 = xl.parse('ГРП')
-    df3 = xl.parse('ТР.Служба')
+    # df3 = xl.parse('ТР.Служба')
     # pprint(df1)
 
 if __name__ == '__main__':
